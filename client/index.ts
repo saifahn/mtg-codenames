ae908c0b77552ac32e8c8dbf28b11595b88da6a7
Sean Li <sean.saifahn.li@gmail.com>
2024-12-02 13:55:08 +0900

    wip: more states for game

--- client/index.ts
+++ client/index.ts
@@ -1,3 +1,5 @@
+import { GameState } from '..'
+
 const ws = new WebSocket('ws://localhost:3000')
 
 ws.onopen = () => {
@@ -10,14 +12,53 @@ ws.onopen = () => {
   ws.send(JSON.stringify(loginRequestMessage))
 }
 
+function isGameState(jsonParsed: any): jsonParsed is GameState {
+  return jsonParsed.game !== undefined
+}
+
 ws.onmessage = (event) => {
   if (typeof event.data !== 'string') {
     console.log('received an unexpected Buffer')
     return
   }
-  const serverState = JSON.parse(event.data)
-  if (serverState.game === null) {
-    console.log('There is no game in progress')
+
+  if (event.data === 'GAME_CREATED') {
+    const startGame = confirm(
+      'A new game was successfully created. Would you like to start?'
+    )
+    if (startGame) {
+      ws.send(JSON.stringify({ action: 'startGame' }))
+    }
+    return
+  }
+
+  if (event.data === 'GAME_STARTED') {
+    console.log('The game has started')
+    return
+  }
+
+  // TODO: validate with a package like zod?
+  const message = JSON.parse(event.data)
+
+  if (isGameState(message)) {
+    if (message.game === null) {
+      const createNewGame = confirm(
+        'There is no game currently in progress. Would you like to create a new game?'
+      )
+
+      if (createNewGame) {
+        ws.send(JSON.stringify({ action: 'createNewGame' }))
+      }
+      return
+    }
+
+    if (message.game.status === 'ready') {
+      const startGame = confirm('There is a game. Would you like to begin?')
+      if (startGame) {
+        ws.send(JSON.stringify({ action: 'startGame ' }))
+      }
+      return
+    }
   }
 }
 
--- index.ts
+++ index.ts
@@ -10,10 +10,49 @@ interface NoGameState {
   game: null
 }
 
-type GameState = NoGameState
+type Team = 'Azorius' | 'Rakdos'
+
+interface BoardSpace {
+  word: string
+  identity: Team | 'assassin' | 'neutral'
+  flipped: boolean
+}
+
+interface GameBaseState {
+  board: BoardSpace[][]
+  turn: Team
+  goesFirst: Team
+  currentTurn: Team
+  status: 'ready' | 'inProgress' | 'finished'
+  // add users
+}
+
+export type GameState = NoGameState | { game: GameBaseState }
 
 const state: GameState = {
   game: null,
+}
+
+function createNewGame() {
+  // load words
+  // split
+  // choose 25 at random
+  // load them into the board
+  // choose which team goesFirst
+  // set current turn to that team
+  // set status to ready
+
+  const game: GameBaseState = {
+    status: 'ready',
+  }
+  state.game = game
+}
+
+function startGame() {
+  const game: GameBaseState = {
+    status: 'inProgress',
+  }
+  state.game = game
 }
 
 const server = Bun.serve({
@@ -44,11 +83,20 @@ const server = Bun.serve({
       try {
         const parsedMsg = JSON.parse(message)
         const { action } = parsedMsg
+
         if (action === 'login') {
           console.log(`LOG: ${parsedMsg.username} has logged in`)
-          if (state.game === null) {
-            socket.send(JSON.stringify(state))
-          }
+          socket.send(JSON.stringify(state))
+        }
+
+        if (action === 'createNewGame') {
+          createNewGame()
+          socket.send('GAME_CREATED')
+        }
+
+        if (action === 'startGame') {
+          startGame()
+          server.publish('game', 'GAME_STARTED')
         }
       } catch (err) {
         console.log(err)
